d3fff38814b0045fbf602abb78312849fa9c8639
Maarten van Gompel <proycon@anaproy.nl>
2020-03-24 14:39:52 +0100

    refactoring

--- deepfrog/tagger.py
+++ deepfrog/tagger.py
@@ -631,165 +631,172 @@ class Tagger:
 
         return dev_evaluation, test_output, test_evaluation
 
+    @staticmethod
+    def argument_parser(parser=None):
+        if parser is None:
+            parser = argparse.ArgumentParser()
+
+        # Required parameters
+        parser.add_argument(
+            "--train_file",
+            default=None,
+            type=str,
+            required=False,
+            help="The file containing the trainings data (two tab separated columns (word, token), one token per line, sentences delimited by an empty line or <utt>)",
+        )
+        parser.add_argument(
+            "--dev_file",
+            default=None,
+            type=str,
+            required=False,
+            help="The file containing the development data (two tab separated columns (word, token), one token per line, sentences delimited by an empty line or <utt>). Evaluation will be performed on this file.",
+        )
+        parser.add_argument(
+            "--test_file",
+            default=None,
+            type=str,
+            required=False,
+            help="The file containing the development data (two tab separated columns (word, token), one token per line, sentences delimited by an empty line or <utt>)",
+        )
+
+        #parser.add_argument(
+        #    "--work_dir",
+        #    default=".",
+        #    type=str,
+        #    required=False,
+        #    help="The directory used for caching fine-tuned models",
+        #)
+        parser.add_argument(
+            "--model_type",
+            default=None,
+            type=str,
+            required=True,
+            help="Model type selected in the list: " + ", ".join(MODEL_CLASSES.keys()),
+        )
+        parser.add_argument(
+            "--pretrained_model",
+            "-m",
+            default=None,
+            type=str,
+            required=True,
+            help="Path to pre-trained model or shortcut name selected in the list: " + ", ".join(ALL_MODELS),
+        )
+        parser.add_argument(
+            "--model_dir",
+            "-d",
+            default=None,
+            type=str,
+            required=True,
+            help="The directory where the fine-tuned model will be stored (and checkpoints) will be written and read from.",
+        )
+        parser.add_argument(
+            "--labels_file",
+            "-l",
+            default=None,
+            type=str,
+            required=False,
+            help="Path to a file holding all labels (plain text, utf-8, one label per line), will be automatically derived from training data if not specified."
+        )
+
+        #Other parameters
+        parser.add_argument(
+            "--config_name", default="", type=str, help="Pretrained config name or path if not the same as pretrained_model"
+        )
+        parser.add_argument(
+            "--tokenizer_name",
+            default="",
+            type=str,
+            help="Pretrained tokenizer name or path if not the same as pretrained_model",
+        )
+        parser.add_argument(
+            "--model_cache_dir",
+            default="",
+            type=str,
+            help="Where do you want to store the pre-trained transformer models downloaded from huggingface? (defaults to huggingface library default)",
+        )
+        parser.add_argument(
+            "--input_cache_dir",
+            default=".",
+            type=str,
+            help="Where do you want to store cached data for the input data? (defaults to current working directory)",
+        )
+        parser.add_argument(
+            "--max_seq_length",
+            default=128,
+            type=int,
+            help="The maximum total input sequence length after tokenization. Sequences longer "
+            "than this will be truncated, sequences shorter will be padded.",
+        )
+        parser.add_argument(
+            "--evaluate_during_training",
+            action="store_true",
+            help="Whether to run evaluation during training at each logging step.",
+        )
+        parser.add_argument(
+            "--do_lower_case", action="store_true", help="Set this flag if you are using an uncased model."
+        )
+
+        parser.add_argument("--per_gpu_train_batch_size", default=8, type=int, help="Batch size per GPU/CPU for training.")
+        parser.add_argument(
+            "--per_gpu_eval_batch_size", default=8, type=int, help="Batch size per GPU/CPU for evaluation."
+        )
+        parser.add_argument(
+            "--gradient_accumulation_steps",
+            type=int,
+            default=1,
+            help="Number of updates steps to accumulate before performing a backward/update pass.",
+        )
+        parser.add_argument("--learning_rate", default=5e-5, type=float, help="The initial learning rate for Adam.")
+        parser.add_argument("--weight_decay", default=0.0, type=float, help="Weight decay if we apply some.")
+        parser.add_argument("--adam_epsilon", default=1e-8, type=float, help="Epsilon for Adam optimizer.")
+        parser.add_argument("--max_grad_norm", default=1.0, type=float, help="Max gradient norm.")
+        parser.add_argument(
+            "--num_train_epochs", default=3.0, type=float, help="Total number of training epochs to perform."
+        )
+        parser.add_argument(
+            "--max_steps",
+            default=-1,
+            type=int,
+            help="If > 0: set total number of training steps to perform. Override num_train_epochs.",
+        )
+        parser.add_argument("--warmup_steps", default=0, type=int, help="Linear warmup over warmup_steps.")
+
+        parser.add_argument("--logging_steps", type=int, default=500, help="Log every X updates steps.")
+        parser.add_argument("--save_steps", type=int, default=500, help="Save checkpoint every X updates steps.")
+        parser.add_argument(
+            "--eval_all_checkpoints",
+            action="store_true",
+            help="Evaluate all checkpoints starting with the same prefix as model_name ending and ending with step number",
+        )
+        parser.add_argument("--no_cuda", action="store_true", help="Avoid using CUDA when available")
+        parser.add_argument(
+            "--overwrite_model_dir", action="store_true", help="Overwrite the content of the output directory"
+        )
+        parser.add_argument(
+            "--overwrite_cache", action="store_true", help="Overwrite the cached training and evaluation sets"
+        )
+        parser.add_argument("--seed", type=int, default=42, help="random seed for initialization")
+
+        parser.add_argument(
+            "--fp16",
+            action="store_true",
+            help="Whether to use 16-bit (mixed) precision (through NVIDIA apex) instead of 32-bit",
+        )
+        parser.add_argument(
+            "--fp16_opt_level",
+            type=str,
+            default="O1",
+            help="For fp16: Apex AMP optimization level selected in ['O0', 'O1', 'O2', and 'O3']."
+            "See details at https://nvidia.github.io/apex/amp.html",
+        )
+        parser.add_argument("--local_rank", type=int, default=-1, help="For distributed training: local_rank")
+        parser.add_argument("--debug","-D", help="Enables debug mode for extra verbose output", action="store_true")
+
+        return parser
 
 def main():
-    parser = argparse.ArgumentParser()
+    parser = Tagger.argument_parser()
 
-    # Required parameters
-    parser.add_argument(
-        "--train_file",
-        default=None,
-        type=str,
-        required=False,
-        help="The file containing the trainings data (two tab separated columns (word, token), one token per line, sentences delimited by an empty line or <utt>)",
-    )
-    parser.add_argument(
-        "--dev_file",
-        default=None,
-        type=str,
-        required=False,
-        help="The file containing the development data (two tab separated columns (word, token), one token per line, sentences delimited by an empty line or <utt>). Evaluation will be performed on this file.",
-    )
-    parser.add_argument(
-        "--test_file",
-        default=None,
-        type=str,
-        required=False,
-        help="The file containing the development data (two tab separated columns (word, token), one token per line, sentences delimited by an empty line or <utt>)",
-    )
-
-    #parser.add_argument(
-    #    "--work_dir",
-    #    default=".",
-    #    type=str,
-    #    required=False,
-    #    help="The directory used for caching fine-tuned models",
-    #)
-    parser.add_argument(
-        "--model_type",
-        default=None,
-        type=str,
-        required=True,
-        help="Model type selected in the list: " + ", ".join(MODEL_CLASSES.keys()),
-    )
-    parser.add_argument(
-        "--pretrained_model",
-        "-m",
-        default=None,
-        type=str,
-        required=True,
-        help="Path to pre-trained model or shortcut name selected in the list: " + ", ".join(ALL_MODELS),
-    )
-    parser.add_argument(
-        "--model_dir",
-        "-d",
-        default=None,
-        type=str,
-        required=True,
-        help="The directory where the fine-tuned model will be stored (and checkpoints) will be written and read from.",
-    )
-    parser.add_argument(
-        "--labels_file",
-        "-l",
-        default=None,
-        type=str,
-        required=False,
-        help="Path to a file holding all labels (plain text, utf-8, one label per line), will be automatically derived from training data if not specified."
-    )
-
-    #Other parameters
-    parser.add_argument(
-        "--config_name", default="", type=str, help="Pretrained config name or path if not the same as pretrained_model"
-    )
-    parser.add_argument(
-        "--tokenizer_name",
-        default="",
-        type=str,
-        help="Pretrained tokenizer name or path if not the same as pretrained_model",
-    )
-    parser.add_argument(
-        "--model_cache_dir",
-        default="",
-        type=str,
-        help="Where do you want to store the pre-trained transformer models downloaded from huggingface? (defaults to huggingface library default)",
-    )
-    parser.add_argument(
-        "--input_cache_dir",
-        default=".",
-        type=str,
-        help="Where do you want to store cached data for the input data? (defaults to current working directory)",
-    )
-    parser.add_argument(
-        "--max_seq_length",
-        default=128,
-        type=int,
-        help="The maximum total input sequence length after tokenization. Sequences longer "
-        "than this will be truncated, sequences shorter will be padded.",
-    )
-    parser.add_argument(
-        "--evaluate_during_training",
-        action="store_true",
-        help="Whether to run evaluation during training at each logging step.",
-    )
-    parser.add_argument(
-        "--do_lower_case", action="store_true", help="Set this flag if you are using an uncased model."
-    )
-
-    parser.add_argument("--per_gpu_train_batch_size", default=8, type=int, help="Batch size per GPU/CPU for training.")
-    parser.add_argument(
-        "--per_gpu_eval_batch_size", default=8, type=int, help="Batch size per GPU/CPU for evaluation."
-    )
-    parser.add_argument(
-        "--gradient_accumulation_steps",
-        type=int,
-        default=1,
-        help="Number of updates steps to accumulate before performing a backward/update pass.",
-    )
-    parser.add_argument("--learning_rate", default=5e-5, type=float, help="The initial learning rate for Adam.")
-    parser.add_argument("--weight_decay", default=0.0, type=float, help="Weight decay if we apply some.")
-    parser.add_argument("--adam_epsilon", default=1e-8, type=float, help="Epsilon for Adam optimizer.")
-    parser.add_argument("--max_grad_norm", default=1.0, type=float, help="Max gradient norm.")
-    parser.add_argument(
-        "--num_train_epochs", default=3.0, type=float, help="Total number of training epochs to perform."
-    )
-    parser.add_argument(
-        "--max_steps",
-        default=-1,
-        type=int,
-        help="If > 0: set total number of training steps to perform. Override num_train_epochs.",
-    )
-    parser.add_argument("--warmup_steps", default=0, type=int, help="Linear warmup over warmup_steps.")
-
-    parser.add_argument("--logging_steps", type=int, default=500, help="Log every X updates steps.")
-    parser.add_argument("--save_steps", type=int, default=500, help="Save checkpoint every X updates steps.")
-    parser.add_argument(
-        "--eval_all_checkpoints",
-        action="store_true",
-        help="Evaluate all checkpoints starting with the same prefix as model_name ending and ending with step number",
-    )
-    parser.add_argument("--no_cuda", action="store_true", help="Avoid using CUDA when available")
-    parser.add_argument(
-        "--overwrite_model_dir", action="store_true", help="Overwrite the content of the output directory"
-    )
-    parser.add_argument(
-        "--overwrite_cache", action="store_true", help="Overwrite the cached training and evaluation sets"
-    )
-    parser.add_argument("--seed", type=int, default=42, help="random seed for initialization")
-
-    parser.add_argument(
-        "--fp16",
-        action="store_true",
-        help="Whether to use 16-bit (mixed) precision (through NVIDIA apex) instead of 32-bit",
-    )
-    parser.add_argument(
-        "--fp16_opt_level",
-        type=str,
-        default="O1",
-        help="For fp16: Apex AMP optimization level selected in ['O0', 'O1', 'O2', and 'O3']."
-        "See details at https://nvidia.github.io/apex/amp.html",
-    )
-    parser.add_argument("--local_rank", type=int, default=-1, help="For distributed training: local_rank")
-    parser.add_argument("--debug","-D", help="Enables debug mode for extra verbose output", action="store_true")
     args = parser.parse_args()
 
     if (
@@ -799,7 +806,7 @@ def main():
         and not args.overwrite_model_dir
     ):
         raise ValueError(
-            "Model output directory ({}) already exists and is not empty. Use --overwrite_model_dir to overcome.".format(
+            "Model output directory ({}) already exists and is not empty. Use --overwrite_model_dir to overwrite.".format(
                 args.model_dir
             )
         )
