5968af6ca520cacac7476b1ac95cb51acc782d90
Maarten van Gompel <proycon@anaproy.nl>
2020-04-14 14:50:22 +0200

    return alignments from format classes

--- deepfrog/formats.py
+++ deepfrog/formats.py
@@ -9,28 +9,39 @@ class TaggerFormat:
     def from_txt(inputfilename: str, outputfilename: str, **kwargs):
         sentenceperline = kwargs.get('sentenceperline',False)
         wrotenewline = False
+        linenr = 1
+        tokennr = 1
+        alignment = []
         with open(inputfilename,'r',encoding='utf-8') as fin:
             with open(outputfilename,'w',encoding='utf-8') as fout:
                 for line in fin:
                     for token in line.split(" "):
                         wrotenewline = False
                         print(token,file=fout)
+                        alignment.append((linenr,tokennr,None))
+                        tokennr += 1
 
                     if (sentenceperline or not line.strip()) and not wrotenewline:
                         fout.write("\n")
+                        linenr += 1
+                        tokennr = 1
                         wrotenewline = True #prevents two successive newlines
+        return alignment
 
 
 
     @staticmethod
     def from_xml(inputfile, outputfilename, **kwargs):
+        alignment = []
         if isinstance(inputfile, folia.Document):
             doc = inputfile
         else:
             doc = folia.Document(file=inputfile)
         with open(outputfilename,'w',encoding='utf-8') as fout:
-            for sentence in doc.sentences():
-                for token in sentence.words():
+            for sentencenr, sentence in enumerate(doc.sentences()):
+                for tokennr, token in enumerate(sentence.words()):
                     print(token, file=fout)
-            fout.write("\n")
+                    alignment.append((sentencenr,tokennr,token.id))
+                fout.write("\n")
+        return alignment
 
