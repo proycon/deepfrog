4c9c1933bb8b851545f8763da989771eb3259323
Maarten van Gompel <proycon@anaproy.nl>
2020-04-29 14:53:02 +0200

    renamed

---
+++ trainers/train-deepfrog-pos-nld.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+mkdir cache
+mkdir ../models
+nextflow run train_tagger.nf --outputdir $(realpath ../models) --model bert-base-dutch-cased --traindata inputdata/frog1.1-pos-nld.train.txt --devdata inputdata/frog1.1-pos-nld.dev.txt --save_steps 250000 --testdata inputdata/frog1.1-pos-nld.test.txt --name bert-pos-cased-deepfrog-nld --cache_dir $(realpath cache) --examplespath $(realpath transformers/examples)  -with-trace
