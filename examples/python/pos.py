b20328aaf8271fa1cb4ccc24fd3e1ab6add5cf34
Maarten van Gompel <proycon@anaproy.nl>
2020-04-29 14:50:42 +0200

    added example

---
+++ examples/python/pos.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+"""
+This is an example using the DeepFrog NER model for Transformers, the DeepFrog software itself is not needed for
+this example.
+
+Automatically downloading the model is implied, so you can use this to get
+started straight away.
+"""
+
+from transformers import AutoTokenizer, AutoModel, pipeline
+
+modelname = "proycon/bert-pos-cased-frog-nld"
+tokenizer = "bert-base-dutch-cased"
+
+#Load manually:
+#tokenizer = Auto#Or use pipelines:Tokenizer.from_pretrained(modelname)
+#model = AutoModel.from_pretrained(modelname)
+
+#Or use pipelines:
+nlp = pipeline('ner', model=modelname, tokenizer=modelname) #you can add ignore_labels=[] or in later versions: group=True
+result = nlp("Ik geef hem een cadeau.")
+print(result)
+result = nlp("Amsterdam is de hoofdstad van Nederland, maar de regering zetelt in Den Haag.")
+print(result)
+
+
