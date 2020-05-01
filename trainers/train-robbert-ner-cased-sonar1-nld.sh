e60faabc234bdbda523b4925ce8b145d3022838e
Maarten van Gompel <proycon@anaproy.nl>
2020-05-01 16:19:19 +0200

    added NER training script

---
+++ trainers/train-robbert-ner-cased-sonar1-nld.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+mkdir cache
+mkdir ../models
+nextflow run train_tagger.nf --outputdir $(realpath ../models) --model pdelobelle/robBERT-base --traindata inputdata/sonar1-ner-nld.train.txt --devdata inputdata/sonar1-ner-nld.dev.txt --testdata inputdata/sonar1-ner-nld.test.txt --name robbert-ner-cased-sonar1-nld --cache_dir $(realpath cache) --examplespath $(realpath transformers/examples)  -with-trace
