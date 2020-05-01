8ae29388d6df1c78ee39a0c04651a3372bb10260
Maarten van Gompel <proycon@anaproy.nl>
2020-05-01 15:05:35 +0200

    added robbert pos training, removed renamed files

--- trainers/train-sonar1-ner-nld.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-mkdir cache
-mkdir ../models
-nextflow run train_tagger.nf --outputdir $(realpath ../models) --model bert-base-dutch-cased --traindata inputdata/sonar1-ner-nld.train.txt --devdata inputdata/sonar1-ner-nld.dev.txt --testdata inputdata/sonar1-ner-nld.test.txt --name bert-ner-cased-sonar1-nld --cache_dir $(realpath cache) --examplespath $(realpath transformers/examples)  -with-trace
