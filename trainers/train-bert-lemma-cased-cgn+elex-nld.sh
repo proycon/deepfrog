6650b6be9211dfdfaa7362a9e912c65f28836291
Maarten van Gompel <proycon@anaproy.nl>
2020-09-08 15:13:32 +0200

    model needs to have a user part now?

--- trainers/train-bert-lemma-cased-cgn+elex-nld.sh
+++ trainers/train-bert-lemma-cased-cgn+elex-nld.sh
@@ -5,4 +5,4 @@ mkdir cache
 mkdir ../models
 source preprocess-lemma-data.sh
 
-nextflow run train_tagger.nf --outputdir $(realpath ../models) --model bert-base-dutch-cased --traindata inputdata/cgn+elex-lemma-nld.train.txt --devdata inputdata/cgn+elex-lemma-nld.dev.txt --testdata inputdata/cgn+elex-lemma-nld.test.txt --name bert-lemma-cased-cgn+elex-nld --cache_dir $(realpath cache) --examplespath $(realpath transformers/examples)  -with-trace
+nextflow run train_tagger.nf --outputdir $(realpath ../models) --model wietsedv/bert-base-dutch-cased --traindata inputdata/cgn+elex-lemma-nld.train.txt --devdata inputdata/cgn+elex-lemma-nld.dev.txt --testdata inputdata/cgn+elex-lemma-nld.test.txt --name bert-lemma-cased-cgn+elex-nld --cache_dir $(realpath cache) --examplespath $(realpath transformers/examples)  -with-trace
