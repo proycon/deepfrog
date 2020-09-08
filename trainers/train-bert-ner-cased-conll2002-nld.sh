30863b4aa9abbfa26d0ac3115ddec50a528bcb07
Maarten van Gompel <proycon@anaproy.nl>
2020-09-08 15:16:33 +0200

    adjust the other scripts as well

--- trainers/train-bert-ner-cased-conll2002-nld.sh
+++ trainers/train-bert-ner-cased-conll2002-nld.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir cache
 mkdir ../models
-nextflow run train_tagger.nf --outputdir $(realpath ../models) --model bert-base-dutch-cased --traindata inputdata/conll2002-ner-nld.train.txt --devdata inputdata/conll2002-ner-nld.dev.txt --testdata inputdata/conll2002-ner-nld.test.txt --name bert-ner-cased-conll2002-nld --cache_dir $(realpath cache) --examplespath $(realpath transformers/examples)  -with-trace
+nextflow run train_tagger.nf --outputdir $(realpath ../models) --model wietsedv/bert-base-dutch-cased --traindata inputdata/conll2002-ner-nld.train.txt --devdata inputdata/conll2002-ner-nld.dev.txt --testdata inputdata/conll2002-ner-nld.test.txt --name bert-ner-cased-conll2002-nld --cache_dir $(realpath cache) --examplespath $(realpath transformers/examples)  -with-trace
