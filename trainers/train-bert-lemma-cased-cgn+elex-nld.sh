85b3a230abc03e78ecccb02771d2c206625e9bb0
Maarten van Gompel <proycon@anaproy.nl>
2020-09-08 12:45:37 +0200

    added lemma training script

---
+++ trainers/train-bert-lemma-cased-cgn+elex-nld.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+mkdir cache
+mkdir ../models
+source preprocess-lemma-data.sh
+
+nextflow run train_tagger.nf --outputdir $(realpath ../models) --model bert-base-dutch-cased --traindata inputdata/cgn+elex-lemma-nld.train.txt --devdata inputdata/cgn+elex-lemma-nld.dev.txt --testdata inputdata/cgn+elex-lemma-nld.test.txt --name bert-lemma-cased-cgn+elex-nld --cache_dir $(realpath cache) --examplespath $(realpath transformers/examples)  -with-trace
